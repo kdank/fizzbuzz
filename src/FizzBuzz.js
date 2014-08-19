e53d9c3d117b1b1acebb172e34469bda3aad724d
Jacob Dankner <pair+jacob@xogrp.com>
2014-08-18 23:05:17 -0400

    Implement Basic Fizzbuzz

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,3 +1,11 @@
-function helloWorld() {
-    return "Hello world!";
+function fizzBuzz(number) {
+  if (number % 3 === 0 && number % 5 === 0) {
+    return 'fizzbuzz';
+  } else if (number % 3 === 0) {
+    return 'fizz';
+  } else if (number % 5 === 0) {
+    return 'buzz';
+  } else {
+    return number;
+  }
 }
