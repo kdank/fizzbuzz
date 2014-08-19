6c811350d132dc5390655c556fd47e429595f89e
Jacob Dankner <pair+jacob@xogrp.com>
2014-08-18 22:53:58 -0400

    end of first tests

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,5 +1,25 @@
-describe("Hello world", function() {
-    it("says hello", function() {
-        expect(helloWorld()).toEqual("Hello world!");
+describe("FizzBuzz", function() {
+  describe("When the number is divisable by three but not five", function() {
+    it("returns the string 'fizz'", function() {
+      expect(fizzBuzz(6)).toEqual("fizz");
     });
+  });
+
+  describe("When the number is divisable by five but not three", function() {
+    it("returns the string 'buzz'", function() {
+      expect(fizzBuzz(10)).toEqual("buzz");
+    });
+  });
+
+  describe("When the number is divisable by three AND five", function() {
+    it("returns the string 'buzz'", function() {
+      expect(fizzBuzz(15)).toEqual("fizzbuzz");
+    });
+  });
+
+  describe("When the number is divisable by neither three NOR five", function() {
+    it("returns the string 'buzz'", function() {
+      expect(fizzBuzz(7)).toEqual(7);
+    });
+  });
 });
